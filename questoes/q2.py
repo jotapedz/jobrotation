3475e60d5fbb52359ba68e23fd0af3b2526e8a3e
João Pedro Lisboa <jotapedro927@gmail.com>
2023-03-08 14:25:24 -0300

    questoões 1 a 5

---
+++ questoes/q2.py
@@ -0,0 +1,20 @@
+def fibonacci(n):
+    #Inicializa a sequência com os valores iniciais
+    sequencia = [0, 1]
+    while len(sequencia) < n:
+        # Calcula o próximo termo da sequência como a soma dos dois termos anteriores
+        proximo = sequencia[-1] + sequencia[-2]
+        sequencia.append(proximo)
+    return sequencia
+
+#Lê o número informado 
+num = int(input('Informe um número: '))
+
+#Calcula a sequência de Fibonacci até o número informado
+seq_fib = fibonacci(num)
+
+#Verifica se o número informado pertence à sequência de Fibonacci
+if num in seq_fib:
+    print(f'O número {num} pertence à sequência de Fibonacci: {seq_fib}')
+else:
+    print(f'O número {num} não pertence à sequência de Fibonacci: {seq_fib}')
---
+++ questoes/q5.py
@@ -0,0 +1,15 @@
+# Definindo a string que será invertida
+string = "Hello World!"
+
+# Criando uma lista vazia para armazenar os caracteres invertidos
+string_invertida = []
+
+# Percorrendo a string de trás para frente e adicionando cada caractere na lista de caracteres invertidos
+for i in range(len(string)-1, -1, -1):
+    string_invertida.append(string[i])
+
+# Transformando a lista de caracteres invertidos em uma string
+string_invertida = ''.join(string_invertida)
+
+# Imprimindo a string invertida
+print(string_invertida)
